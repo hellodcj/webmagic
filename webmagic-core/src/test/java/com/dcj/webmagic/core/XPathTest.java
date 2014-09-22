8108741d00749d49138fac93a1442158bb3d2b42
hellodcj <hellodcj@163.com>
2014-09-22 17:46:02 +0800

    xpathTest

---
+++ webmagic-core/src/test/java/com/dcj/webmagic/core/XPathTest.java
@@ -0,0 +1,20 @@
+package com.dcj.webmagic.core;
+import org.junit.Test;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Request;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.downloader.Downloader;
+import us.codecraft.webmagic.downloader.HttpClientDownloader;
+
+public class XPathTest {
+
+	@Test
+	public void test(){
+//		Request request = new Request("http://my.oschina.net/flashsword/blog");
+//		Downloader downloader = new HttpClientDownloader();
+//		Task task = new Spider(pageProcessor);
+//		Page page = downloader.download(request, this);	
+	}
+}
